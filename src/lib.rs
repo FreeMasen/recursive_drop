8457d4e87dd8455089b05aadd966ea86d67f9a7b
rfm <r@robertmasen.pizza>
2019-12-17 20:01:06 -0600

    add 3 variant

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,19 @@
 
 struct Start {
-    start: Nested
+    start: Nested,
+}
+
+struct Start3 {
+    start: Nested3,
 }
 enum Nested {
     With(Box<Value>),
+    Without
+}
+
+enum Nested3 {
+    With(Box<Value3>),
+    Other(Box<Other>),
     Without
 }
 
@@ -12,10 +22,28 @@ struct Value {
     next: Nested,
 }
 
+struct Value3 {
+    data: usize,
+    next: Nested3,
+}
+
+struct Other {
+    data: Value3,
+    n: usize,
+}
+
 impl Start {
     fn nested(n: usize) -> Self {
         Self {
             start: Nested::nested(n),
+        }
+    }
+}
+
+impl Start3 {
+    fn nested(n: usize) -> Self {
+        Self {
+            start: Nested3::nested(n),
         }
     }
 }
@@ -34,15 +62,49 @@ impl Nested {
     }
 }
 
+impl Nested3 {
+    fn nested(n: usize) -> Self {
+        let mut inner = Self::Without;
+        for i in 0..n {
+            let v = Value3 {
+                data: i,
+                next: inner,
+            };
+            if i % 2 == 0 {
+                inner = Self::With(Box::new(v));
+            } else {
+                let o = Other {
+                    data: v,
+                    n: i,
+                };
+                inner = Self::Other(Box::new(o));
+            }
+        }
+        inner
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
     #[test]
-    fn find_deepest() {
+    fn find_deepest2() {
+        // mbp i9 6 core 16gb ram
+        // 18702 max
         for i in 0..std::usize::MAX {
             dbg!(i);
             {
                 let _thing = Start::nested(i);
+            }
+        }
+    }
+    #[test]
+    fn find_deepest3() {
+        //16364
+        for i in 0..std::usize::MAX {
+            dbg!(i);
+            {
+                let _thing = Start3::nested(i);
             }
         }
     }
